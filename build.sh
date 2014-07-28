1a903f535ef8f10a82300226d0ec0ee8cf879fc6
uberj <jaxbr503@gmail.com>
2014-07-28 15:12:04 -0400

    changes

--- build.sh
+++ build.sh
@@ -36,8 +36,9 @@ MOZ_DIB_IMAGE_ARCH=${MOZ_DIB_IMAGE_ARCH:-amd64}
 export MOZ_DIB_IMAGE_TYPE=${MOZ_DIB_IMAGE_TYPE:-qcow2}
 export MOZ_DIB_IS_PUBLIC=${MOZ_DIB_IS_PUBLIC:-true}
 export MOZ_KEYSTONE_PROFILE_PATH=${MOZ_KEYSTONE_PROFILE_PATH}
-MOZ_LOCAL_ELEMENTS_PATH=${MOZ_WORKING_DIR}/elements/:${MOZ_WORKING_DIR}/heat-templates/hot/software-config/elements/
-MOZ_DIB_ELEMENTS="${MOZ_DIB_DISTRO} vm mozpuppet-bootstrap"
+MOZ_LOCAL_ELEMENTS_PATH=${MOZ_WORKING_DIR}/elements/:${MOZ_WORKING_DIR}/heat-templates/hot/software-config/elements/:${MOZ_WORKING_DIR}/tripleo-image-elements/elements/:${MOZ_WORKING_DIR}/diskimage-builder/elements/
+#MOZ_DIB_ELEMENTS="${MOZ_DIB_DISTRO} vm mozpuppet-bootstrap heat-config-script"
+MOZ_DIB_ELEMENTS="${MOZ_DIB_DISTRO} vm heat-config-script os-collect-config os-refresh-config os-apply-config"
 
 [ -n "$MOZ_DIB_DISTRO" ] || die "Please specifify a distro by setting MOZ_DIB_DISTRO"
 [ -n "$MOZ_DIB_RELEASE" ] || die "Please specifify the $MOZ_DIB_DISTRO release by setting MOZ_DIB_RELEASE"
